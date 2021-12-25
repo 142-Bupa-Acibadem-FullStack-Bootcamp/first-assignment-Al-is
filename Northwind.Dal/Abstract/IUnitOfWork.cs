b52cb91a1d56fbc48b3c6da5950b1d66be87f907
Ali Can CUCU <cucu.alican@gmail.com>
2021-12-25 13:43:39 +0300

    NorthwindBackend
    25.12.2021

---
+++ Northwind.Dal/Abstract/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using Northwind.Entity.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Dal.Abstract
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<T> GetRepository<T>() where T : EntityBase;
+
+        //Transactinon =>onaylama -- Başladı mı ?
+        bool BeginTransaction();
+        //Geri Alındı mı ?
+        bool RollBackTransaction();
+        //Etkilenen satır sayısı
+        int SaveChanges();
+    }
+}
